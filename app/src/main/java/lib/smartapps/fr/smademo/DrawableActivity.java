959e5a983515d536d0bd3fccb6d06b8e8d1cc1ed
Vincent Chann <vc@smartapps.fr>
2016-08-12 11:22:04 +0200

    Clear code

--- app/src/main/java/lib/smartapps/fr/smademo/DrawableActivity.java
+++ app/src/main/java/lib/smartapps/fr/smademo/DrawableActivity.java
@@ -24,18 +24,38 @@ public class DrawableActivity extends AppCompatActivity {
         ImageView imageView2 = (ImageView) findViewById(R.id.image_2);
         ImageView imageView3 = (ImageView) findViewById(R.id.image_3);
 
-        imageView1.setBackground(assetManager.getDrawable("tinder.png"));
-        imageView2.setBackground(assetManager.getDrawable("tinder.png").filter("#000000"));
-        imageView3.setBackground(assetManager.getDrawable("tinder.png").alpha(100));
+        if (imageView1 != null) {
+            imageView1.setBackground(assetManager.getDrawable("tinder.png"));
+        }
+        if (imageView2 != null) {
+            imageView2.setBackground(assetManager.getDrawable("tinder.png").filter("#000000"));
+        }
+        if (imageView3 != null) {
+            imageView3.setBackground(assetManager.getDrawable("tinder.png").alpha(100));
+        }
 
         // buttons
         Button buttonView = (Button) findViewById(R.id.button);
         ToggleButton toggleButtonView = (ToggleButton) findViewById(R.id.toggle_button);
         ImageButton imageButtonView = (ImageButton) findViewById(R.id.image_button);
 
-        buttonView.setBackground(assetManager.getStateListDrawable().focused("tinder.png").pressed("tinder.png").inverse(assetManager.getDrawable("tinder.png").filter("#000000")));
-        toggleButtonView.setBackground(assetManager.getStateListDrawable().checked(assetManager.getDrawable("tinder.png").alpha(100)).inverse("tinder.png"));
-        imageButtonView.setBackground(assetManager.getStateListDrawable().focused("pikachu.png").pressed("pikachu.png").inverse(assetManager.getDrawable("pikachu.png").alpha(100)));
+        if (buttonView != null) {
+            buttonView.setBackground(assetManager.getStateListDrawable()
+                    .focused("tinder.png")
+                    .pressed("tinder.png")
+                    .inverse(assetManager.getDrawable("tinder.png").filter("#000000")));
+        }
+        if (toggleButtonView != null) {
+            toggleButtonView.setBackground(assetManager.getStateListDrawable()
+                    .checked(assetManager.getDrawable("tinder.png").alpha(100))
+                    .inverse("tinder.png"));
+        }
+        if (imageButtonView != null) {
+            imageButtonView.setBackground(assetManager.getStateListDrawable()
+                    .focused("pikachu.png")
+                    .pressed("pikachu.png")
+                    .inverse(assetManager.getDrawable("pikachu.png").alpha(100)));
+        }
 
 
     }
